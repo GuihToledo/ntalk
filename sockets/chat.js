5aeb2c5c12a45ce3495a72c6a0c014e63ef59cf2
Guilherme Toledo Flores Bardeli <guibardeli@gmail.com>
2015-05-25 18:26:34 -0300

    Implementação de salas de chat

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,10 +1,40 @@
 module.exports = function(io) {
-	var sockets = io.sockets;
+	var crypto = require('crypto')
+		, md5 = crypto.createHash('md5')
+		sockets = io.sockets;
+
 	sockets.on('connection', function (client) {
-		client.on('send-server', function (data) {
-			var msg = "<b>"+data.nome+":</b> "+data.msg+"<br>";
-			client.emit('send-client', msg);
-			client.broadcast.emit('send-client', msg);
+		var session = client.handshake.session
+		, usuario = session.usuario;
+		client.on('send-server', function (msg) {
+			var msg = "<b>"+ usuario.nome +":</b> "+ msg +"<br>";
+			client.get('sala', function(erro, sala) {
+				var data = {email: usuario.email, sala: sala};
+				client.broadcast.emit('new-message', data);
+				sockets.in(sala).emit('send-client', msg);
+				console.log("SALAAAA ENTRADA ->>" + sala);
+			});
+		});
+
+		client.on('join', function(sala) {
+			console.log("Entrou Join");
+			if(sala) {
+				sala = sala.replace('?','');
+			} else {
+				var timestamp = new Date().toString();
+				var md5 = crypto.createHash('md5');
+				sala = md5.update(timestamp).digest('hex');
+			}
+			client.set('sala', sala);
+			console.log("SALA JOIN->>" + sala);
+			client.join(sala);
+		});
+
+		client.on('disconnect', function () {
+			client.get('sala', function(erro, sala) {
+				console.log("SALAAAA ->>" + sala);
+				client.leave(sala);
+			});
 		});
 	});
 }
