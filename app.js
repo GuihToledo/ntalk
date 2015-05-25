17f67b63a5b9adf46d63548a50a91e020f3776ad
Guilherme Toledo Flores Bardeli <guibardeli@gmail.com>
2015-05-25 16:32:23 -0300

    Implementação de websocket e real-time

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 var express = require('express')
+, app = express()
 , load = require('express-load')
-, app = express();
+, error = require('./middleware/error')
+, server = require('http').createServer(app)
+, io = require('socket.io').listen(server);
 
-  var app = express();
 
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.cookieParser('ntalk'));
-  app.use(express.session());
-  app.use(express.json());
-  app.use(express.urlencoded());
-  app.use(express.static(__dirname + '/public'));
+const KEY = 'ntalk.sid', SECRET = 'ntalk';
+var cookie = express.cookieParser(SECRET)
+, store = new express.session.MemoryStore()
+, sessOpts = {secret: SECRET, key: KEY, store: store}
+, session = express.session(sessOpts);
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(cookie);
+app.use(session);
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+app.use(error.notFound);
+app.use(error.serverError);
 
   load('models')
     .then('controllers')
     .then('routes')
     .into(app);
+  load('sockets')
+    .into(io);
 
-  app.listen(3000, function(){
-    console.log("Ntalk no ar.");
-  });
+server.listen(3000, function(){
+   console.log("Ntalk no ar.");
+});
